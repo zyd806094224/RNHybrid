463d70dde5abbe73bc9c76b60882be87f3a591e4
zhaoyudong <zhaoyudong@staff.hexun.com>
2026-04-24 12:37:48 +0800

    兼容适配鸿蒙版本pushy

--- harmony/entry/hvigorfile.ts
+++ harmony/entry/hvigorfile.ts
@@ -1,8 +1,7 @@
 import { hapTasks } from '@ohos/hvigor-ohos-plugin';
 import { hapPlugin } from "@hadss/hmrouter-plugin";
-import { reactNativeUpdatePlugin } from 'pushy/hvigor-plugin';
 
 export default {
     system: hapTasks,  /* Built-in plugin of Hvigor. It cannot be modified. */
-    plugins: [hapPlugin(), reactNativeUpdatePlugin()]         /* Custom plugin to extend the functionality of Hvigor. */
+    plugins: [hapPlugin()]         /* Custom plugin to extend the functionality of Hvigor. */
 }
